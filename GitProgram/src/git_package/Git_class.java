64a8b88d7839a678e5cd1ca8bd2f03acf2bff3eb
kaushal <kshrivastav517@gmail.com>
2015-03-25 12:23:11 +0530

    java saving message to git

---
+++ GitProgram/src/git_package/Git_class.java
@@ -0,0 +1,14 @@
+package git_package;
+
+public class Git_class 
+{
+	 int a=10,b=20,c;
+	public static void main(String[] args) 
+	{
+		 Git_class obj=new Git_class();
+	   System.out.println("The numbers given are a="+obj.a+" b="+obj.b);
+	   obj.c=obj.a+obj.b;
+	   System.out.println("The result of the sum is c = "+obj.c);
+	}
+
+}
